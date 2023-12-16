8ab72acd5a8a1d177a52150d84867bd51318b422
ooortcloud <rlaeoals2504@gmail.com>
2023-12-16 23:54:46 +0900

    1216 update truncate

--- crud.py
+++ crud.py
@@ -12,16 +12,21 @@ def insert_data(conn, cur, data):
         cur.execute(sql, (row[0].value, row[1].value, row[2].value, row[3].value, row[4].value))  # 동적 SQL 문을 반복 실행. 파이썬이 알아서 해당 column에 적절한 타입으로 변환하여 대입함.
     conn.commit()  # DB 데이터 삽입 확정
 
+def truncate(conn, cur):
+    sql = 'truncate table bookshelf'
+    cur.execute(sql)
+    conn.commit()
+
 def find_data():
     # DB 연결
-    conn = pymysql.connect (host='localhost', port=3307, user='root', password='1234', 
-                        db='ajoubooking', charset='utf8')
+    conn = pymysql.connect (host='localhost', user='root', password='1234', db='ajoubooking', charset='utf8')
 
     try:
         with conn.cursor() as cur:  # DB와 상호작용하기 위해 연결해주는 cursor 객체
             wb = load_workbook("C:/Users/rlaeo/develop-world/Python-world/AJOU Booking/AJOU Booking.xlsx", data_only=True)  # 경로에 있는 xlsx 파일을 로드한다. (옵션은 재읽기가 가능하게 설정)
             ws = wb['시트1']  # 데이터를 가져올 시트를 선택
             find_all_data = ws.rows  # 시트의 모든 셀들을 순차적으로 읽어와서 튜플 형식으로 메모리에 저장
+            truncate(conn, cur)
             insert_data(conn, cur, find_all_data)      
     
     finally:
